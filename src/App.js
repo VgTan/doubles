d5a4bf4b1ebe2c26f9ca1d79c826f98fd6a988ef
Tiaaa <chyntia2104irawan@gmail.com>
2024-08-04 01:12:22 +0700

    feat:

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import About from "./components/About";
 import Home from "./components/Home";
 import Package from "./components/Package";
 import Navbar from "./components/Navbar";
+import {HelmetProvider} from "react-helmet-async"
 import "./App.css";
-
+const helmetContext = {}
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/package" element={<Package />} />
-          {/* <Route path="/contactus" element={<ContactUs />} /> */}
-        </Routes>
-      </Router>
+      <HelmetProvider context={helmetContext}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/package" element={<Package />} />
+          </Routes>
+        </Router>
+      </HelmetProvider>
     </>
   );
 }
