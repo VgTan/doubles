94317dd3bfc1161c2aa98da72324c4e9fc4b2dc1
Tiaaa <chyntia2104irawan@gmail.com>
2024-08-04 01:24:17 +0700

    feat:

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+
 export default function SEO({ title, description, name, type }) {
   return (
     <Helmet>
@@ -7,17 +8,19 @@ export default function SEO({ title, description, name, type }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       {/* End standard metadata tags */}
-      {/* Facebook tags */}
+
+      {/* Open Graph / Facebook */}
       <meta property="og:type" content={type} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      {/* End Facebook tags */}
-      {/* Twitter tags */}
-      <meta name="twitter:creator" content={name} />}
+      {/* End Open Graph / Facebook */}
+
+      {/* Twitter */}
+      <meta name="twitter:creator" content={name} />
       <meta name="twitter:card" content={type} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      {/* End Twitter tags */}
+      {/* End Twitter */}
     </Helmet>
   );
 }
