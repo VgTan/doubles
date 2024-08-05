60b59412455c0bc8230aa2f99ddd16e690696e67
Tiaaa <chyntia2104irawan@gmail.com>
2024-08-05 23:08:42 +0700

    Update Package.js

--- src/components/Package.js
+++ src/components/Package.js
@@ -198,43 +198,6 @@ function Package() {
               </Reveal>
               <Reveal>
                 <div
-                  class="bg-white p-8 pb-20 h-full max-w-full rounded-2xl hover:shadow-xl hover:shadow-[#0a42515f] flex flex-col relative"
-                  href="#"
-                >
-                  <img
-                    src="/images/package/tiktok.png"
-                    class="shadow rounded-lg overflow-hidden border"
-                  />
-                  <div class="mt-6 sm:mt-8">
-                    <h1 className="text-xl sm:text-2xl font-semibold pb-3 text-[#0A4251]">
-                      Tiktok Package
-                    </h1>
-                    <div>
-                      <ul className="list-disc list-outside pl-5 text-sm text-[#0A4251]">
-                        <li>15 Post/month (adjustable)</li>
-                        <li>Talent 2-3 Person</li>
-                        <li>Content Writing</li>
-                        <li>Research</li>
-                        <li>Monthly Report</li>
-                        <li>
-                          Production Session (concept, shooting, and editing)
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div class="absolute bottom-0 flex justify-between left-1/2 -translate-x-1/2 w-full px-10 pb-6">
-                    <button
-                      onClick={handleRedirect}
-                      class="inline-flex items-center rounded-md border border-transparent bg-[#0A4251] px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-[#1b363e]"
-                    >
-                      Start Creating
-                    </button>
-                    <p className="text-2xl self-center font-bold">3 Mio.</p>
-                  </div>
-                </div>
-              </Reveal>
-              <Reveal>
-                <div
                   class="bg-white p-8 pb-20 max-w-full rounded-2xl hover:shadow-xl hover:shadow-[#0a42515f] flex flex-col relative"
                   href="#"
                 >
