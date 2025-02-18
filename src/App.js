1a09c46e0e79e9672218c028e73ceaa5e664bcb8
Tiaa21 <132942390+Tiaa21@users.noreply.github.com>
2025-02-18 17:15:33 +0700

    Revert "Backend"
    This reverts commit 56b096e026949a162f539bdf3bc0969e739e25a1.

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import Package from "./components/Package";
+import Navbar from "./components/Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/package" element={<Package />} />
+          {/* <Route path="/contactus" element={<ContactUs />} /> */}
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
